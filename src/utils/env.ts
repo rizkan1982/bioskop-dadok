51cf8dedb03a213f9eb403aaa045886a8b5beb65
rizkan1982 <123041936+rizkan1982@users.noreply.github.com>
2025-10-13 21:14:08 +0700

    Update env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -7,13 +7,14 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_TMDB_ACCESS_TOKEN: z.string().min(1),
-    NEXT_PUBLIC_SUPABASE_URL: z.url().min(1),
+    NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: z.string().min(1),
     NEXT_PUBLIC_CAPTCHA_SITE_KEY: z.string().min(1),
-    NEXT_PUBLIC_AVATAR_PROVIDER_URL: z.url().min(1),
+    NEXT_PUBLIC_AVATAR_PROVIDER_URL: z.string().url(),
     NEXT_PUBLIC_ADSENSE_CLIENT_ID: z.string().optional(),
     NEXT_PUBLIC_ADSENSE_SLOT_ID: z.string().optional(),
   },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION || process.env.NODE_ENV === "test",
   experimental__runtimeEnv: {
     NEXT_PUBLIC_TMDB_ACCESS_TOKEN: process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
