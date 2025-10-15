23b266b9b09909f8dd57d1c94ab56dc181d17476
rizkan1982 <123041936+rizkan1982@users.noreply.github.com>
2025-10-15 23:14:18 +0700

    Update players.ts

--- src/utils/players.ts
+++ src/utils/players.ts
@@ -12,30 +12,87 @@ import { PlayersProps } from "@/types";
 export const getMoviePlayers = (id: string | number, startAt?: number): PlayersProps[] => {
   return [
     {
+      title: "VidSrc Prime",
+      source: `https://vidsrc.xyz/embed/movie/${id}`,
+      recommended: true,
+      fast: true,
+      ads: false,
+    },
+    {
+      title: "VidSrc.to",
+      source: `https://vidsrc.to/embed/movie/${id}`,
+      recommended: true,
+      fast: true,
+      ads: true,
+    },
+    {
+      title: "VidSrc Pro",
+      source: `https://vidsrc.net/embed/movie/${id}`,
+      recommended: true,
+      fast: true,
+      ads: false,
+    },
+    {
       title: "VidLink",
       source: `https://vidlink.pro/movie/${id}?player=jw&primaryColor=006fee&secondaryColor=a2a2a2&iconColor=eefdec&autoplay=false&startAt=${startAt || ""}`,
-      recommended: true,
       fast: true,
       ads: true,
       resumable: true,
     },
     {
-      title: "VidLink 2",
+      title: "VidLink Alt",
       source: `https://vidlink.pro/movie/${id}?primaryColor=006fee&autoplay=false&startAt=${startAt}`,
-      recommended: true,
       fast: true,
       ads: true,
       resumable: true,
     },
     {
-      title: "<Embed>",
+      title: "Embed.su",
       source: `https://embed.su/embed/movie/${id}`,
+      fast: true,
+      ads: true,
+    },
+    {
+      title: "VidSrc.cc V2",
+      source: `https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=false`,
+      ads: true,
+    },
+    {
+      title: "VidSrc.cc V3",
+      source: `https://vidsrc.cc/v3/embed/movie/${id}?autoPlay=false`,
+      fast: true,
+      ads: true,
+    },
+    {
+      title: "VidSrc ICU",
+      source: `https://vidsrc.icu/embed/movie/${id}`,
       ads: true,
     },
     {
       title: "SuperEmbed",
       source: `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1`,
       fast: true,
+      ads: true,
+    },
+    {
+      title: "AutoEmbed",
+      source: `https://autoembed.co/movie/tmdb/${id}`,
+      fast: true,
+      ads: true,
+    },
+    {
+      title: "AutoEmbed Alt",
+      source: `https://player.autoembed.cc/embed/movie/${id}`,
+      ads: true,
+    },
+    {
+      title: "2Embed",
+      source: `https://www.2embed.cc/embed/${id}`,
+      ads: true,
+    },
+    {
+      title: "MoviesAPI",
+      source: `https://moviesapi.club/movie/${id}`,
       ads: true,
     },
     {
@@ -46,54 +103,6 @@ export const getMoviePlayers = (id: string | number, startAt?: number): PlayersP
     {
       title: "NontonGo",
       source: `https://www.nontongo.win/embed/movie/${id}`,
-      ads: true,
-    },
-    {
-      title: "AutoEmbed 1",
-      source: `https://autoembed.co/movie/tmdb/${id}`,
-      fast: true,
-      ads: true,
-    },
-    {
-      title: "AutoEmbed 2",
-      source: `https://player.autoembed.cc/embed/movie/${id}`,
-      ads: true,
-    },
-    {
-      title: "2Embed",
-      source: `https://www.2embed.cc/embed/${id}`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 1",
-      source: `https://vidsrc.xyz/embed/movie/${id}`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 2",
-      source: `https://vidsrc.to/embed/movie/${id}`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 3",
-      source: `https://vidsrc.icu/embed/movie/${id}`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 4",
-      source: `https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=false`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 5",
-      source: `https://vidsrc.cc/v3/embed/movie/${id}?autoPlay=false`,
-      recommended: true,
-      fast: true,
-      ads: true,
-    },
-    {
-      title: "MoviesAPI",
-      source: `https://moviesapi.club/movie/${id}`,
       ads: true,
     },
   ];
@@ -118,30 +127,87 @@ export const getTvShowPlayers = (
 ): PlayersProps[] => {
   return [
     {
+      title: "VidSrc Prime",
+      source: `https://vidsrc.xyz/embed/tv/${id}/${season}/${episode}`,
+      recommended: true,
+      fast: true,
+      ads: false,
+    },
+    {
+      title: "VidSrc.to",
+      source: `https://vidsrc.to/embed/tv/${id}/${season}/${episode}`,
+      recommended: true,
+      fast: true,
+      ads: true,
+    },
+    {
+      title: "VidSrc Pro",
+      source: `https://vidsrc.net/embed/tv/${id}/${season}/${episode}`,
+      recommended: true,
+      fast: true,
+      ads: false,
+    },
+    {
       title: "VidLink",
       source: `https://vidlink.pro/tv/${id}/${season}/${episode}?player=jw&primaryColor=f5a524&secondaryColor=a2a2a2&iconColor=eefdec&autoplay=false&startAt=${startAt || ""}`,
-      recommended: true,
       fast: true,
       ads: true,
       resumable: true,
     },
     {
-      title: "VidLink 2",
+      title: "VidLink Alt",
       source: `https://vidlink.pro/tv/${id}/${season}/${episode}?primaryColor=f5a524&autoplay=false&startAt=${startAt}`,
-      recommended: true,
       fast: true,
       ads: true,
       resumable: true,
     },
     {
-      title: "<Embed>",
+      title: "Embed.su",
       source: `https://embed.su/embed/tv/${id}/${season}/${episode}`,
+      fast: true,
+      ads: true,
+    },
+    {
+      title: "VidSrc.cc V2",
+      source: `https://vidsrc.cc/v2/embed/tv/${id}/${season}/${episode}?autoPlay=false`,
+      ads: true,
+    },
+    {
+      title: "VidSrc.cc V3",
+      source: `https://vidsrc.cc/v3/embed/tv/${id}/${season}/${episode}?autoPlay=false`,
+      fast: true,
+      ads: true,
+    },
+    {
+      title: "VidSrc ICU",
+      source: `https://vidsrc.icu/embed/tv/${id}/${season}/${episode}`,
       ads: true,
     },
     {
       title: "SuperEmbed",
       source: `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1&s=${season}&e=${episode}`,
       fast: true,
+      ads: true,
+    },
+    {
+      title: "AutoEmbed",
+      source: `https://autoembed.co/tv/tmdb/${id}-${season}-${episode}`,
+      fast: true,
+      ads: true,
+    },
+    {
+      title: "AutoEmbed Alt",
+      source: `https://player.autoembed.cc/embed/tv/${id}/${season}/${episode}`,
+      ads: true,
+    },
+    {
+      title: "2Embed",
+      source: `https://www.2embed.cc/embedtv/${id}&s=${season}&e=${episode}`,
+      ads: true,
+    },
+    {
+      title: "MoviesAPI",
+      source: `https://moviesapi.club/tv/${id}-${season}-${episode}`,
       ads: true,
     },
     {
@@ -152,54 +218,6 @@ export const getTvShowPlayers = (
     {
       title: "NontonGo",
       source: `https://www.NontonGo.win/embed/tv/${id}/${season}/${episode}`,
-      ads: true,
-    },
-    {
-      title: "AutoEmbed 1",
-      source: `https://autoembed.co/tv/tmdb/${id}-${season}-${episode}`,
-      fast: true,
-      ads: true,
-    },
-    {
-      title: "AutoEmbed 2",
-      source: `https://player.autoembed.cc/embed/tv/${id}/${season}/${episode}`,
-      ads: true,
-    },
-    {
-      title: "2Embed",
-      source: `https://www.2embed.cc/embedtv/${id}&s=${season}&e=${episode}`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 1",
-      source: `https://vidsrc.xyz/embed/tv/${id}/${season}/${episode}`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 2",
-      source: `https://vidsrc.to/embed/tv/${id}/${season}/${episode}`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 3",
-      source: `https://vidsrc.icu/embed/tv/${id}/${season}/${episode}`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 4",
-      source: `https://vidsrc.cc/v2/embed/tv/${id}/${season}/${episode}?autoPlay=false`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 5",
-      source: `https://vidsrc.cc/v3/embed/tv/${id}/${season}/${episode}?autoPlay=false`,
-      recommended: true,
-      fast: true,
-      ads: true,
-    },
-    {
-      title: "MoviesAPI",
-      source: `https://moviesapi.club/tv/${id}-${season}-${episode}`,
       ads: true,
     },
   ];
